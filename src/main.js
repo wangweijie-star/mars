5e3d21a768591eeaa0880b8f0f12d4e8064c104b
wangweijie-star <1061424574@qq.com>
2020-12-22 20:11:19 +0800

    wwj第二次上传

--- src/main.js
+++ src/main.js
@@ -5,9 +5,11 @@ import store from './store'
 // 引入矢量图标
 import './assets/font/iconfont.css'
 // 引入want框架
-import { Tabbar, TabbarItem, Search } from 'vant'
+import { Tabbar, TabbarItem, Search, Swipe, SwipeItem } from 'vant'
 // 引入全局样式
 import './assets/css/style.scss'
+// 引入移动端适配定义字体大小
+import 'amfe-flexible'
 
 const app = createApp(App)
 app.use(store)
@@ -15,4 +17,6 @@ app.use(router)
 app.use(Tabbar)
 app.use(TabbarItem)
 app.use(Search)
+app.use(Swipe)
+app.use(SwipeItem)
 app.mount('#app')
