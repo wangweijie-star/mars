4be371638c938400902d01c9b01a894d8443b01f
Yan Kun <2844881713@qq.com>
2020-12-23 08:33:20 +0800

    登录页

--- src/main.js
+++ src/main.js
@@ -5,7 +5,14 @@ import store from './store'
 // 引入矢量图标
 import './assets/font/iconfont.css'
 // 引入want框架
-import { Tabbar, TabbarItem, Search } from 'vant'
+import {
+  Tabbar,
+  TabbarItem,
+  Search,
+  Field,
+  NavBar,
+  Button
+} from 'vant'
 // 引入全局样式
 import './assets/css/style.scss'
 
@@ -15,4 +22,8 @@ app.use(router)
 app.use(Tabbar)
 app.use(TabbarItem)
 app.use(Search)
+app.use(Field)
+app.use(NavBar)
+app.use(Button)
+
 app.mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -7,35 +7,69 @@ const routes = [
     children: [
       {
         path: '/',
-        redirect: '/index'
+        redirect: '/index',
+        meta: {
+          keepAlive: true
+        }
       },
       {
         path: '/index',
-        component: () => import('../views/Index.vue')
+        component: () => import('../views/Index.vue'),
+        meta: {
+          keepAlive: true
+        }
       },
       {
         path: '/find',
-        component: () => import('../views/Find.vue')
+        component: () => import('../views/Find.vue'),
+        meta: {
+          keepAlive: true
+        }
       },
       {
         path: '/publish',
-        component: () => import('../views/Publish.vue')
+        component: () => import('../views/Publish.vue'),
+        meta: {
+          keepAlive: true
+        }
       },
       {
+        // 消息
         path: '/message',
-        component: () => import('../views/Message.vue')
+        component: () => import('../views/Message.vue'),
+        meta: { requiresAuth: true, keepAlive: true }
       },
       {
+        // 我的
         path: '/mine',
-        component: () => import('../views/Mine.vue')
+        component: () => import('../views/Mine.vue'),
+        meta: { requiresAuth: true, keepAlive: true }
       }
     ]
+  },
+  {
+    path: '/login',
+    component: () => import('../views/Login.vue')
   }
+
 ]
 
 const router = createRouter({
   history: createWebHashHistory(),
   routes
+})
+router.beforeEach((to, from, next) => {
+  // console.log(to.meta)
+  if (to.meta.requiresAuth) {
+    console.log(to.meta.requiresAuth)
+    if (localStorage.token) {
+      next()
+    } else {
+      next('/login')
+    }
+  } else {
+    next()
+  }
 })
 
 export default router
